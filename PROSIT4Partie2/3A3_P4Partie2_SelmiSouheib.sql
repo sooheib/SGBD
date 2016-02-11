c8de1e95ec57ef2221a7ae462a2472fd70217fc6
sooheib <soosel93@gmail.com>
2016-02-11 11:48:52 +0100

    PROSIT SGBD

--- PROSIT4Partie2/3A3_P4Partie2_SelmiSouheib.sql
+++ PROSIT4Partie2/3A3_P4Partie2_SelmiSouheib.sql
@@ -1,24 +1,23 @@
-
 1. Afficher les noms en majuscules, les prénoms en minuscule relatifs à toutes les
 personnes enregistrés.
-select UPPER(nom) , LOWER(prenom) from PERSONNES
+select UPPER(NOM) , LOWER(PRENOM) from PERSONNES
 ------------------------------------------------------------------
 2. Afficher le total du kilométrage de tous les véhicules. Le résultat doit être arrondi.
-select ROUND(SUM(kilometrage)) from vehicules
+select ROUND(SUM(KILOMETRAGE)) from VEHICULES
 --------------------------------------------------------------------
 3. Afficher tous les véhicules vendus pendant l’année dernière.
-select * from vehicules where immatriculation in (  select immatriculation from CONTRAT_VENTE where EXTRACT(YEAR FROM DATE_VENTE) = (EXTRACT(YEAR FROM sysdate))-1 )
+select * from VEHICULES where IMMATRICULATION in (  select IMMATRICULATION from CONTRAT_VENTE where EXTRACT(YEAR FROM DATE_VENTE) = (EXTRACT(YEAR FROM SYSDATE))-1)
 --------------------------------------------------------------------
 
 4. Afficher le nombre de véhicules vendus pendant le premier trimestre de l’année 2014.
-select * from vehicules where immatriculation in (  select immatriculation from CONTRAT_VENTE where EXTRACT(month FROM DATE_VENTE) > 3)
+select * from VEHICULES where IMMATRICULATION in (  select IMMATRICULATION from CONTRAT_VENTE where EXTRACT(month FROM DATE_VENTE) > 3)
 ---------------------------------------------------------------------
 5. Afficher la liste des véhicules dont la date de possession ne dépasse pas 6 mois.
 Select * from VEHICULES  where MONTHS_BETWEEN( SYSDATE, DATE_MISE_CIRCULATION )<7
 ----------------------------------------------------------------------
 6. Afficher tous les véhicules dont la date de début de possession dépasse la date de
 vente au plus de 4 jours.
-select * from vehicules where immatriculation in (  select immatriculation from CONTRAT_VENTE where DATE_VENTE-(select DATE_MISE_CIRCULATION from vehicules )>4 )
+select * from VEHICULES where IMMATRICULATION in (  select IMMATRICULATION from CONTRAT_VENTE where DATE_VENTE-(select DATE_MISE_CIRCULATION from VEHICULES )>4 )
 ------------------------------------------------------------------------
 7. Afficher pour chaque véhicule son immatriculation, sa puissance fiscale et le tarif des
 vignettes à payer. Le prix des vignettes est calculé en se basant sur la puissance fiscale
