7678627f2b9470d3bdc09f86f378a712074de738
sooheib <soosel93@gmail.com>
2016-02-11 10:02:38 +0100

    PROSIT SGBD

--- PROSIT5/3A3_P5_SelmiSouheib.sql
+++
@@ -1,4 +0,0 @@
-select * from employees where job_id in('SA_MAN','AD_VP',null)
-select * from employees where job_id not  in('SA_MAN','AD_VP',null)
-
-
---
+++ PROSIT5/cours.sql
@@ -0,0 +1,26 @@
+select * from employees where job_id in('SA_MAN','AD_VP',null)
+select * from employees where job_id not  in('SA_MAN','AD_VP',null)
+
+NOT IN vs. NOT EXISTS vs. LEFT JOIN / IS NULL: Oracle
+
+Concat: ||
+length
+trim
+ltrim ('n' from 'nation') - RTRIM('123000', '0') Result: '123'
+rtrim('char')
+lpad():SELECT LPAD('azerty', 4, 'x');  -- retourne 'azer'
+rpad()
+round()
+trunc
+to_date: SELECT MONTHS_BETWEEN
+   (TO_DATE('02-02-1995','MM-DD-YYYY'),
+    TO_DATE('01-01-1995','MM-DD-YYYY') ) "Months"
+    FROM DUAL;
+to_char:TO_CHAR(sysdate, 'yyyy/mm/dd')-Result: '2003/07/09'
+to_number
+
+
+
+
+
+
