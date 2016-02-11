48e399b9d8b7b2121ef43d22c8a2a7c50bc28b5b
sooheib <soosel93@gmail.com>
2016-02-11 10:07:01 +0100

    PROSIT SGBD

--- PROSIT5/cours.sql
+++ PROSIT5/cours.sql
@@ -11,11 +11,13 @@ rtrim('char')
 lpad():SELECT LPAD('azerty', 4, 'x');  -- retourne 'azer'
 rpad()
 round()
-trunc
+trunc:TRUNC(125.815, 1)
+Result: 125.8
 to_date: SELECT MONTHS_BETWEEN
    (TO_DATE('02-02-1995','MM-DD-YYYY'),
     TO_DATE('01-01-1995','MM-DD-YYYY') ) "Months"
     FROM DUAL;
+    select extract(year from sysdate) as year
 to_char:TO_CHAR(sysdate, 'yyyy/mm/dd')-Result: '2003/07/09'
 to_number
 
